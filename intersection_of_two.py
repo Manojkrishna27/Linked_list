a7f8dc38f3d0fd5d40c056eddc97a0aad00ef043
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-04-09 06:44:59 +0530

    intersection of Two node

---
+++ intersection_of_two.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+def intersection_of_list(head1,head2):
+    p1=head1
+    p2=head2
+
+    while p1!=p2:
+        p1=p1.next if p1 else head2 # if not in p1 move to second list
+        p2=p2.next if p2 else head1  # if not p2 move to first list
+
+    return p1
+
+
+head1=Node(10)
+head1.next=Node(15)
+head1.next.next=Node(20)
+head1.next.next.next=Node(25)
+head2=Node(10)
+head2.next=head1.next # here is  the intersection
+node=intersection_of_list(head1,head2)
+if node:
+    print("intersection at",node.data)
+else:
+    print("No intersection")
