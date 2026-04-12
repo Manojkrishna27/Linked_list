8f9174f29ae77ccf64d5217e69f0f147183d5231
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-04-12 07:53:27 +0530

    double linked list

--- dd_delete_tail.py
+++ dd_delete_tail.py
@@ -4,15 +4,17 @@ class Node:
         self.prev=None
         self.next=None
 
-def delete_head(head):
+def delete_tail(head):
     if head is None:
         return None
     if head.next is None:
         return None
-    
-    head=head.next # change head to head.next (1 to 2)
-    head.prev=None # and now head will be 2 you need to connect to None
+    temp=head
 
+    while temp.next:
+        temp=temp.next
+
+    temp.prev.next=None # disconnect
     return head
 def printlist(head):
     temp=head
@@ -20,9 +22,12 @@ def printlist(head):
         print(temp.data,end="->")
         temp=temp.next
     print("Null")
-
 head=Node(1)
 head.next=Node(2)
+head.next.prev=head
 head.next.next=Node(3)
+head.next.next.prev=head.next
 head.next.next.next=Node(4)
-printlist(delete_head(head))
+head.next.next.next.prev=head.next.next
+printlist(head) # original 
+printlist(delete_tail(head)) # after deleting
