81c289097a773774ba12cd85a591d672bcb8ddb4
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-04-17 13:42:40 +0530

    search_element & position in ddl

---
+++ search_element.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
+def search_element_ddl(head,key):
+    temp=head
+    while temp:
+        if temp.data==key: # take temp for reuse compare to target if found true
+            return True
+        temp=temp.next
+    return False # else false
+def search_position(head,key):
+    temp=head
+    pos=1
+    while temp:
+        if temp.data==key:
+            return pos
+        temp=temp.next
+        pos+=1
+    return -1
+head=Node(10)
+head.next=Node(15)
+head.next.prev=head
+head.next.next=Node(20)
+head.next.next.prev=head.next
+head.next.next.next=Node(30)
+head.next.next.next.prev=head.next.next
+print(search_element_ddl(head,20)) # true
+print(search_element_ddl(head,2)) # false
+print(search_position(head,30))
